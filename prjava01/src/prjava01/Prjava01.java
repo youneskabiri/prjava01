1442bf33239d5dc1443a19c34f011b182987b1b7
Younes Kabiri <youneskabiri17@gmail.com>
2018-05-16 22:33:47 +0200

    Versió 0.1 del projecte prjava01

---
+++ prjava01/src/prjava01/Prjava01.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava01;
+
+import java.io.*;
+
+/**
+ *
+ * @author youneskabiri1718daw2vib
+ */
+public class Prjava01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");
+            bw.newLine();
+            bw.write("  <head>");
+            bw.newLine();
+            bw.write("    <title>");
+            bw.newLine();
+            bw.write("      Nova p&agrave;gina  web");
+            bw.newLine();
+            bw.write("    </title>");
+            bw.newLine();
+            bw.write("  </head>");
+            bw.newLine();
+            bw.write("  <body>");
+            bw.newLine();
+            bw.write("    Nova p&agrave;gina web");
+            bw.newLine();
+            bw.write("  </body>");
+            bw.newLine();
+            bw.write("</html>");
+            bw.newLine();
+            bw.close();
+        }
+    }
+}
